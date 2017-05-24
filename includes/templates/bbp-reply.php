26b31af7988c9894a5b7ca7455c2c0458d943659
Christoph Herbst <chris.p.herbst@gmail.com>
2017-05-24 13:54:54 +0200

    get correct bbp reply id for like

--- includes/templates/bbp-reply.php
+++ includes/templates/bbp-reply.php
@@ -24,11 +24,11 @@ function bplike_bbp_reply_button() {
         !in_array($post->post_type, bp_like_get_settings('bp_like_post_types')))
         return;
 
-    $vars = bp_like_get_template_vars( get_the_ID(), 'bbp_reply' );
+    $vars = bp_like_get_template_vars( bbp_get_reply_id(), 'bbp_reply' );
     extract( $vars );
 
     ?>
-        <a class="bbp-reply <?php echo $classes ?>" id="bp-like-bbp-reply-<?php echo get_the_ID(); ?>"
+        <a class="bbp-reply <?php echo $classes ?>" id="bp-like-bbp-reply-<?php echo bbp_get_reply_id(); ?>"
            title="<?php echo $title; ?>" data-like-type="bbp_reply">
             <span class="like-text"><?php echo bp_like_get_text( 'like' ); ?></span>
             <span class="unlike-text"><?php echo bp_like_get_text( 'unlike' ); ?></span>
@@ -36,7 +36,7 @@ function bplike_bbp_reply_button() {
         </a>
     <?php
 
-    view_who_likes( get_the_ID(), 'bbp_reply');
+    view_who_likes( bbp_get_reply_id(), 'bbp_reply');
 
 }
 add_action('bbp_theme_after_reply_content', 'bplike_bbp_reply_button');
