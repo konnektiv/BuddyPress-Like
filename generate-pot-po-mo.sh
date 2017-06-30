0b9c1b83ce6a6814d7818ee7b619dc4b16fa40a3
René Schodder <rene@konnektiv.de>
2017-06-30 15:08:09 +0200

    adden German translation to Buddypress-Like

---
+++ generate-pot-po-mo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+namespace=${PWD##*/}
+language="de_DE"
+
+#generate POT file from source
+find . -name "*.php" > filelist
+xgettext --keyword=__ --keyword=_e --keyword=__n --files-from=filelist --language=PHP -o languages/${namespace}.pot
+rm -f filelist
+
+cd languages
+touch ${namespace}-${language}.po
+
+#update German PO file from POT
+msgmerge --update --no-fuzzy-matching --backup=off ${namespace}-${language}.po ${namespace}.pot
+
+#update MO file from PO file
+msgcat ${namespace}-${language}.po | msgfmt -o ${namespace}-${language}.mo -
