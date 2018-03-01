367b012d2952acd3621275a0d08c9d8fcd417689
Christoph Herbst <chris.p.herbst@gmail.com>
2018-03-01 10:32:57 +0100

    update translation generator to include all languages

--- generate-pot-po-mo.sh
+++ generate-pot-po-mo.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-namespace=${PWD##*/}
-language="de_DE"
+TEXTDOMAIN=${PWD##*/}
 
 #generate POT file from source
 find . -name "*.php" > filelist
-xgettext --keyword=__ --keyword=_e --keyword=__n:1,2 --files-from=filelist --language=PHP -o languages/${namespace}.pot
+xgettext --keyword=__ --keyword=_e --keyword=_n:1,2 --keyword=_x:1,2c \
+		 --keyword=_ex:1,2c --files-from=filelist --from-code=UTF-8 --language=PHP -o languages/$TEXTDOMAIN.pot
 rm -f filelist
 
 cd languages
-touch ${namespace}-${language}.po
+for filename in ./*.po; do
+    #update PO files from POT
+    msgmerge --update --no-fuzzy-matching --backup=off $filename $TEXTDOMAIN.pot
 
-#update German PO file from POT
-msgmerge --update --no-fuzzy-matching --backup=off ${namespace}-${language}.po ${namespace}.pot
+    #update MO file from PO file
+    msgcat $filename | msgfmt -o $(basename "$filename" .po).mo -
 
-#update MO file from PO file
-msgcat ${namespace}-${language}.po | msgfmt -o ${namespace}-${language}.mo -
+done
